879ef97e2e2ee9751d91cc21d9e34340ea9a1537
tomoaki-kimura <tkim19750404@gmail.com>
2023-10-19 08:15:53 +0900

    エラーメッセージとバリデーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 20 }
   validates :description, allow_blank: true, length: { minimum: 3, maximum: 500 }
+  validates :start_time, presence: true
 
   enum status: { not_started: 0, in_progress: 1, closed: 2 }
 end
